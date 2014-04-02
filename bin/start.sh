46d40ad9b2061a58d193f784f540e3b4c971aa27
Brian Luan <brian.luan@migo.tv>
2014-04-02 14:59:33 +0800

    add 32618DEVICE_INSPECTOR_DIR for jar file when execute start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -2,5 +2,5 @@
 
 mkdir -p run logs
 
-nohup java -Dconfig=$DEVICE_INSPECTOR_DIR/config.properties -jar bin/*jar > $DEVICE_INSPECTOR_DIR/logs/server.log 2>&1 &
+nohup java -Dconfig=$DEVICE_INSPECTOR_DIR/config.properties -jar $DEVICE_INSPECTOR_DIR/bin/*jar > $DEVICE_INSPECTOR_DIR/logs/server.log 2>&1 &
 echo $! > $DEVICE_INSPECTOR_DIR/run/run.pid
